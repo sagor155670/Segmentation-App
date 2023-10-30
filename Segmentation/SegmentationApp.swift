395d72c2760a9da820d260757edb826b28841947
sagor155670 <sagor@braincraftapps.com>
2023-10-30 16:02:19 +0600

    Initial Commit

---
+++ Segmentation/SegmentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentationApp.swift
+//  Segmentation
+//
+//  Created by Saiful Islam Sagor on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SegmentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
